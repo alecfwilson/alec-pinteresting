52d412c91044cfe569a287aad5fa8d93c7e36813
Alec Wilson <alec.f.wilson@gmail.com>
2014-08-13 20:05:00 -0700

    added page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home' #creates root_path
   get "about" => "pages#about" #creates about_path
+  get "search" => "pages#search"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
